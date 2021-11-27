fa4b0060e3d512f31ee6caf420659da1fcaffa37
anh <w3bdev1999@gmail.com>
2021-11-27 23:10:01 +0700

    e nhat anh chinh sua nhe 1 ti

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
+require_once "./commons/utils.php";
+require_once "dao/pdo.php";
 $url = isset($_GET['url']) ? $_GET['url'] : '/';
 switch ($url) {
     case '/':
         require_once "site/business/homepage.php";
         home();
         break;
-
+    case 'lien-he':
+        require_once "site/business/contact.php";
+        contact();
+        break;
     default:
         # code
         break;
